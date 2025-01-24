88270e91f5a460c2aa540f2d2645735677225e9d
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2025-01-24 17:07:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// let div1 = document.getElementById("element");
+// let remove = div.children[1]; 
+// div.removeChild (remove);
+// console.log(div);
+
+// let div = document.getElementById("element");
+// let newp = document.createElement("h1");
+// let addtext = document.createTextNode("Hello Pakistan");
+// div.appendChild(newp);
+// newp.appendChild(addtext);
+// let replace = div.children[0];
+// div.insertBefore(newp , replace);
+// console.log(div);
+
+
+// //object
+// const information = {
+//     name: "Maham AraB" , 
+//     fatherName: "AbduL Razzaque" , 
+//     rollNo: "278838" ,
+// }
+
+// document.write(information.name , "<br>");
+// document.write(information.fatherName , "<br>");
+// document.write(information.rollNo , "<br>");
+
+
+// const Cheez = {
+//     cheez1: "Rite" ,
+//     cheez2: "HumtUm" , 
+//     cheez3: "Crazy Dip" ,
+// }
+
+// document.write(Cheez.cheez1 , "<br>");
+// document.write(Cheez.cheez2 , "<br>");
+// document.write(Cheez.cheez3 , "<br>");
